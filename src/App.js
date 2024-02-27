cda1eede19d77798c8b34e8e8ff5fba31b07f9c6
Aika Weidman <119600413+aweidman110@users.noreply.github.com>
2024-02-26 22:47:57 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,30 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/aweidman110" target="_blank">
-          {" "}
-          Aika Weidman
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/aweidman110/react-wk5-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a> 💻
-      </footer>
+      <div className="container">
+        <h1>Weather App 🌤️</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/aweidman110"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Aika Weidman
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/aweidman110/react-wk5-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          💻
+        </footer>
+      </div>
     </div>
   );
 }
