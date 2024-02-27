6e9c5c40dab724e490a4c084d3e7b066d0d888ba
Aika Weidman <119600413+aweidman110@users.noreply.github.com>
2024-02-26 22:20:55 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/aweidman110" target="_blank">
+          {" "}
+          Aika Weidman
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/aweidman110/react-wk5-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a> 💻
+      </footer>
+    </div>
+  );
 }
