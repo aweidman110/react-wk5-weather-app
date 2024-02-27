9358bee26b6c6992a9665722d31a5065baa016a0
Aika Weidman <119600413+aweidman110@users.noreply.github.com>
2024-02-26 23:30:59 -0500

    Added the basic html structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App 🌤️</h1>
-        <button className="btn btn-primary">Hello</button>
+  
+        <Weather />
+        
         <footer>
           This project was coded by{" "}
           <a
